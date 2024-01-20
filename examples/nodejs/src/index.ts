79ed798b87e4099268c7693bfeba4dae2c5615fe
MANIAHMED <abdulmani11111111@gmail.com>
2024-01-21 01:09:39 +0500

    wip:adding examples

---
+++ examples/nodejs/src/index.ts
@@ -0,0 +1,9 @@
+// import { OrderBookApi, SupportedChainId } from 'pro-analytics-sdk'
+
+// See more examples in /examples/cra
+// ;(async function () {
+//   const orderBookApi = new OrderBookApi({ chainId: SupportedChainId.LIGHTLINK_PHOENIX_MAINNET })
+
+//   const order = await orderBookApi.getVersion()
+
+// })()
